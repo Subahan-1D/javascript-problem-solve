d7c43336965c731acaf2d8e8330fcdb957784685
subahan ali <subahanislam523@gmail.com>
2024-09-17 21:46:06 +0600

    fast problem solve

---
+++ js-problem-solving-for-coding-interview-main/problem1.js
@@ -0,0 +1,13 @@
+// 1. **Rotate an Array**
+//    Write a function that rotates an array k times. Each rotation moves the last element of the array to the front.
+//    Example: `rotateArray([1, 2, 3, 4, 5], 2)` → `[4, 5, 1, 2, 3]`
+
+
+
+function rotateArray(arr, k) {
+  const n = arr.length;
+  k = k % n;
+  return arr.slice(-k).concat(arr.slice(0,n-k))
+}
+const result = rotateArray([1,2,3,4,5],2)
+console.log(result)
