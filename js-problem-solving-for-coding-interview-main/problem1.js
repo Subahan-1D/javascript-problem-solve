41e354a83406bfcdd711c3c2397bc2c266d0ed5f
subahan ali <subahanislam523@gmail.com>
2024-09-27 20:12:10 +0600

    probelem 3

--- js-problem-solving-for-coding-interview-main/problem1.js
+++ js-problem-solving-for-coding-interview-main/problem1.js
@@ -2,19 +2,6 @@
 //    Write a function that rotates an array k times. Each rotation moves the last element of the array to the front.
 //    Example: `rotateArray([1, 2, 3, 4, 5], 2)` → `[4, 5, 1, 2, 3]`
 
-// function rotateArray(arr, k) {
-//     const n = arr.length;
-//     // If k is greater than the length of the array, we only need to rotate k % n times
-//     k = k % n;
-//     // Splice the last k elements and concatenate them at the front
-//     return arr.slice(-k).concat(arr.slice(0, n - k));
-// }
-
-// // Example usage
-// const result = rotateArray([1, 2, 3, 4, 5], 2);
-// console.log(result); // Output: [4, 5, 1, 2, 3]
-
-
 function rotateArray(arr, k) {
   const n = arr.length;
   k = k % n;
@@ -23,3 +10,5 @@ function rotateArray(arr, k) {
 
 const result = rotateArray([1, 2, 3, 4, 5,], 2);
 console.log(result);
+
+
---
+++ js-problem-solving-for-coding-interview-main/problem2.js
@@ -0,0 +1,8 @@
+// problem 2 
+// *** Hello Word print *
+function greeting(parameterVariable) {
+  // This line prints 'Hello, World!' to the console:
+  console.log("Hello Word");
+  console.log(parameterVariable);
+}
+greeting = "Welcome to 10 Days of JavaScript!";
